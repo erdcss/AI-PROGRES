c3a869d706a89baf7e3681e43422d2d5b569c6d4
erdcss <erdemcalisgan122@gmail.com>
2026-09-16 23:42:40 +0300

    fix: enable MARKT-GO upload retry and image guard

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot, type Root } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { initDevStability } from "./lib/dev-stability";
 import "./lib/exact-bulk-upload-guard";
+import "./lib/marktgo-upload-guard";
 import App from "./App";
 import { queryClient } from "./lib/queryClient";
 import "./index.css";
